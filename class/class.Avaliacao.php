fd81b6be05d43b28531983cd20fee1ac43816393
mageeus <mateusk747@gmail.com>
2023-06-15 14:18:54 -0300

    provavelmente ja ta tudo certo

--- class/class.Avaliacao.php
+++ class/class.Avaliacao.php
@@ -4,14 +4,36 @@ require_once("class.BancoDeDados.php");
 class Avaliacao extends BancoDeDados
 {
 
+    public function listarAvaliacao($idavaliacao)
+    {
+        $listar = $this->retornaArray("select * from avaliacao where idavaliacao = " . $idavaliacao);
+        return $listar;
+    }
     public function listarAvaliacoes()
     {
-        $listar = $this->executarConsulta("select * from avaliacao av left outer join aluno a on av.idavaliacao = a.idaluno");
+        $listar = $this->executarConsulta("select * from avaliacao av left outer join aluno a on av.idaluno = a.idaluno left join disciplina d on av.iddisciplina = d.iddisciplina");
         //"select * from disciplina d left outer join aluno a on d.iddisciplina = a.idaluno");
         //"select * from avaliacao av left outer join aluno a on av.idavaliacao = a.idaluno"
         return $listar;
     }
 
+    public function incluirAvaliacao($idaluno, $iddisciplina, $nota)
+    {
+        $incluir = $this->executarConsulta("insert into avaliacao(idaluno, iddisciplina, nota) values($idaluno, $iddisciplina, $nota)");
+        return $incluir;
+    }
+
+    public function alterarAvaliacao($idavaliacao, $nota)
+    {
+        $alterar = $this->executarConsulta("update avaliacao set nota = '" . $nota . "' where idavaliacao = " . $idavaliacao);
+        return $alterar; 
+    }
+
+    public function excluirAvaliacao($idavaliacao)
+    {
+        $excluir = $this->executarConsulta("delete from avaliacao where idavaliacao = " . $idavaliacao);
+        return $excluir;
+    }
 }
 
 $avaliacao = new Avaliacao();
--- form_avaliacao.php
+++ form_avaliacao.php
@@ -1,6 +1,8 @@
 <?php
 require_once("class/class.Login.php");
+require_once("class/class.Aluno.php");
 require_once("class/class.Avaliacao.php");
+require_once("class/class.Disciplina.php");
 require_once("header.php");
 $obj_login->revalidarLogin();
 ?>
@@ -13,7 +15,7 @@ $obj_login->revalidarLogin();
         <br />
         <table>
             <tr>
-                <td>idavalicao</td>
+                <td>idavaliacao</td>
                 <td>nmaluno</td>
                 <td>dsdisciplina</td>
                 <td>nota</td>
@@ -24,22 +26,83 @@ $obj_login->revalidarLogin();
 
             foreach ($registros as $linha) {
                 echo '<tr>';
-                echo '    <td>' . $linha['idavaliacao'] . '</td>';
+                echo '    <td>    <a href=form_avaliacao.php?alterar=' . $linha['idavaliacao'] . '>' . $linha['idavaliacao'] . '</a> </td>';
                 echo '    <td>' . $linha['nmaluno'] . '</td>';
                 echo '    <td>' . $linha['dsdisciplina'] . '</td>';
-                echo '    <td>' . '<a href=form_avaliacao.php?alterar=' . $linha['nota'] . '>' . $linha['nota'] . '</a> </td>';
+                echo '    <td>' . $linha['nota'] . '</td>';
                 echo '</tr>';
             }
 
             ?>
         </table>
         <?php
-        if(isset($_GET['alterar'])){
-        ?>
-    <hr>
-    <form action="form_avaliacao.php" method="POST">
-        aluno: <input name="nmaluno" type="text" readonly value="<?php echo $_GET['alterar'] ?>">
-        disciplina: 
-        }
+        if (isset($_GET['alterar'])) {
+            $seleciona = $avaliacao->listarAvaliacao($_GET['alterar']);
+            $selecionaAluno = $aluno->listarAluno($seleciona[0]['idaluno']);
+            $selecionaDisciplina = $disciplina->listarDisciplina($seleciona[0]['iddisciplina']);
+            ?>
+            <hr>
+            <form action="form_avaliacao.php" method="POST">
+                Aluno: <input name="nmaluno" type="text" readonly value="<?php echo $selecionaAluno[0]['nmaluno'] ?>">
+                Disciplina: <input name="nmaluno" type="text" readonly
+                value="<?php echo $selecionaDisciplina[0]['dsdisciplina'] ?>">
+                Nota: <input name="nota" type="number" max="10" value="<?php echo $seleciona[0]['nota'] ?>">
+                <input type="hidden" name="idavaliacao"value=" <?php echo $seleciona[0]['idavaliacao']?>" />
+                <input type="submit" name="comando" value="alterar" />
+                <input type="submit" name="comando" value="excluir" />
+            </form>
+            <?php } ?>
+        <hr>
+        ÁREA PRA REGISTRAR NOTAS
+        <hr>
+        <form action="form_avaliacao.php" method="POST">
+            <select name="idaluno">
+                <?php $registrosAluno = $aluno->listarAlunos();
+                foreach ($registrosAluno as $linha) {
+                    echo "<option value='" . $linha['idaluno'] . "'>" . $linha['nmaluno'] . "</option>";
+                }
+                ?>
+            </select>
+            <select name="iddisciplina">
+                <?php $registrosDisciplina = $disciplina->listarDisciplinas();
+                foreach ($registrosDisciplina as $linha) {
+                    echo "<option value='" . $linha['iddisciplina'] . "'>" . $linha['dsdisciplina'] . "</option>";
+                }
+                ?>
+            </select>
+            Nota: <input name="nota" type="number" max="10">
+            <input type="submit" name="comando" value="registrar" required />
+        </form>
 
+        <?php
+        if (isset($_POST['comando']) && ($_POST['comando'] == "registrar")) {
+            $idaluno = $_POST['idaluno'];
+            $iddisciplina = $_POST['iddisciplina'];
+            $nota = $_POST['nota'];
+            var_dump($idavaliacao);
+
+            if ($avaliacao->incluirAvaliacao($idaluno, $iddisciplina, $nota)) {
+                header("location:form_avaliacao.php");
+            }
+        } else if (isset($_POST['comando']) && ($_POST['comando'] == "alterar")) {
+            $avaliacao->alterarAvaliacao($_POST['idavaliacao'], $_POST['nota']);
+            header("location:form_avaliacao.php");
+        } else if (isset($_POST['comando']) && ($_POST['comando'] == "excluir")) {
+            $avaliacao->excluirAvaliacao($_POST['idavaliacao']);
+            header("location:form_avaliacao.php");
+        }
+        ?>
+        <?php
+        //$avaliacao->alterarAvaliacao($seleciona[0]['idavaliacao'], 3)
+        //var_dump($_POST['nota']);
+        /*
+        var_dump($_GET['alterar']);
+        echo "<br>";
+        var_dump($aluno->listarAluno($_GET['alterar']));
+        echo "<br>";
+        var_dump($aluno->listarAluno("17"));
+        echo "<br>";
+        var_dump($selecionaAluno[0]['idaluno']);
+        */
+        ?>
 </body>
